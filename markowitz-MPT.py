ca3bdf65a77e4e3a5ce94287568777fcafdc2606
Dragan <dragankantar@outlook.com>
2020-11-03 00:30:50 +0800

    add data pipeline

--- markowitz-MPT.py
+++ markowitz-MPT.py
@@ -1,10 +1,41 @@
-import cvxpy as cp
+import numpy as np
+import pandas as pd
+from pandas import datetime
+from datetime import timedelta
+import matplotlib.pyplot as plt
+import yfinance as yf
+import cvxpy as cvx
 
-w = cp.Variable(n)
-gamma = cp.Parameter(nonneg=True)
-returns = mu.T*w 
-risk = cp.quad_form(w, Sigma)
-objective = cp.Maximize(ret - gamma*risk)
-constraints =  [cp.sum(w) == 1, w >= 0]
-cp.Problem(objective, constraints)
+def markowitz_MPT(mu, cov, gamma)
+    w = cvx.Variable(len(ret))
+    gamma = cvx.Parameter(nonneg=True)
+    returns = mu * w.T 
+    risk = cvx.quad_form(w, cov)
+
+    objective = cvx.Maximize(returns - gamma*risk)
+    constraints =  [cvx.sum(w) == 1, w >= 0]
+    problem = cvx.Problem(objective, constraints)
+
+    problem.solve()
+
+    return np.array(w.value.flat).round(3)
+
+tickers = 'SPY, DJI, STI, HSI'
+start = '2010-1-1'
+end = '2020-1-1'
+
+data = yf.download(tickers, start, end)
+
+# getting mu
+
+# getting cov
+
+markowitz_MPT()
+
+
+
+
+
+
+
 
