3f3980e53e8aee79e033654d47f4b3b994ba7cfe
Dragan <dragankantar@outlook.com>
2020-11-05 02:00:28 +0800

    fix typos, add plot loop

--- markowitz-MPT.py
+++ markowitz-MPT.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
 import datetime
-from datetime import timedelta
 import matplotlib.pyplot as plt
 import yfinance as yf
 import cvxpy as cvx
 
-def markowitz_MPT(mu, cov, gamma)
-    w = cvx.Variable(len(ret))
+def markowitz_MPT(mu, covmtx, gamma):
+    w = cvx.Variable(len(mu))
     gamma = cvx.Parameter(nonneg=True)
-    returns = mu * w.T 
-    risk = cvx.quad_form(w, cov)
+    returns = mu @ w.T 
+    risk = cvx.quad_form(w, covmtx)
 
     objective = cvx.Maximize(returns - gamma*risk)
     constraints =  [cvx.sum(w) == 1, w >= 0]
@@ -20,22 +19,25 @@ def markowitz_MPT(mu, cov, gamma)
 
     return np.array(w.value.flat).round(3)
 
-tickers = 'SPY, DJI, STI, HSI'
+tickers = 'SPY, DJI, STI'
 start = '2010-1-1'
 end = '2020-1-1'
 
 data = yf.download(tickers, start, end)
-data['Close']
 
 r = data['Close'].pct_change()
-covmtx = np.cov(r.T)
+r = r.iloc[1:]
+mu = np.mean(r)
+covmtx = np.cov(r, rowvar = False)
 
-markowitz_MPT(r, covmtx, 0.2)
+markowitz_MPT(mu, covmtx, 0.2)
 
-
-
-
-
-
-
+for i in range(r.shape[1]):
+    plt.subplot(r.shape[1], 1, i+1)
+    plt.plot(r.iloc[0:, i])
+    plt.xlabel('Time') 
+    plt.ylabel('%s Daily Returns' % r.columns.values[i])
+    
+plt.tight_layout()
+plt.show() 
 
