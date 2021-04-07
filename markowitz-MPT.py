6425ab9765632f5c1b1384c6b6d42cd82e8a4410
Dragan <dragan@u.yale-nus.edu.sg>
2021-04-07 11:33:12 +0800

    fix data prep, rename vars, clean code

--- markowitz-MPT.py
+++ markowitz-MPT.py
@@ -5,38 +5,37 @@ import datetime
 import yfinance as yf
 import cvxpy as cvx
 
-def markowitz_MPT(mu, covmtx, gamma):
-    w = cvx.Variable(len(mu))
+def markowitz_MPT(mean, matrix, gamma):
+    w = cvx.Variable(len(mean))
     gamma = cvx.Parameter(nonneg=True, value=gamma)
-    returns = mu @ w.T 
-    risk = cvx.quad_form(w, covmtx)
+    returns = mean @ w.T
+    risk = cvx.quad_form(w, matrix)
 
     objective = cvx.Maximize(returns - gamma*risk)
-    constraints =  [cvx.sum(w) == 1, w >= 0]
+    constraints = [cvx.sum(w) == 1, w >= 0]
     problem = cvx.Problem(objective, constraints)
-
     problem.solve()
 
     return np.array(w.value.flat).round(3)
 
-tickers = 'SPY, DJI, STI'
-start = '2010-1-1'
+tickers = 'MSFT, SPY'
+begin = '2010-1-1'
 end = '2018-1-1'
+gamma = 0.2 #risk aversion parameter
 
-data = yf.download(tickers, start, end)
+data = yf.download(tickers, begin, end)
 
-r = data['Close'].pct_change().iloc[1:]
-mu = np.mean(r).to_numpy()
-covmtx = np.cov(r, rowvar = False)
+returns = data['Adj Close'].pct_change()
+mean_returns = returns.mean()
+covariance_matrix = returns.cov()
 
-markowitz_MPT(mu, covmtx, 0.2)
+markowitz_MPT(mean_returns, covariance_matrix, gamma)
 
-for i in range(r.shape[1]):
-    plt.subplot(r.shape[1], 1, i+1)
-    plt.plot(r.iloc[0:, i])
-    plt.xlabel('Time') 
-    plt.ylabel('%s Daily Returns' % r.columns.values[i])
-    
+for i in range(returns.shape[1]):
+    plt.subplot(returns.shape[1], 1, i+1)
+    plt.plot(returns.iloc[0:, i])
+    plt.xlabel('Time')
+    plt.ylabel('%s Daily Returns' % returns.columns.values[i])
+
 plt.tight_layout()
-plt.show() 
-
+plt.show()
