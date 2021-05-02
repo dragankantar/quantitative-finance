e5479ab50c33fa2847c42355a4a04f5c0ad3f08e
Dragan <dragan@u.yale-nus.edu.sg>
2021-05-03 01:59:53 +0800

    define class, __init__, data, jb test methods

--- YSC4228/Final/scratch.py
+++ YSC4228/Final/scratch.py
@@ -1,5 +1,9 @@
 #get_dollar_bars.py --file e_mini_sp_series.csv –-dollar_size 18000000
 import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
 
 def enable_parsing(parser):
     """
@@ -16,7 +20,16 @@ parser = argparse.ArgumentParser()
 parser = enable_parsing(parser)
 args = parser.parse_args()
 
+class Bar():
+    def __init__(self, file, dollar_size):
+        self.file = file
+        self.dollar_size = dollar_size
 
+    def data(self):
+        data = pd.read_csv(self.file, index_col='Time', parse_dates=['Time']).dropna()
+        return data
 
-
-
+    def jarque_bera_test(self, dataframe):
+        close = dataframe["Close"].pct_change()[1:]
+        test = stats.jarque_bera(close)
+        return test.statistic, test.pvalue
