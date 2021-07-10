9d0715cd7a57cea9f1a185295f69d89dc4031b3d
Dragan <dragan@u.yale-nus.edu.sg>
2021-07-10 19:52:53 +0800

    add plot_daily_returns method; fix dates in fetching data

--- public_asset_analytics/return_analytics.py
+++ public_asset_analytics/return_analytics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import datetime
 import yfinance as yf
 
@@ -11,7 +12,7 @@ class ReturnAnalytics:
         self.period_begin = datetime.strptime(period_begin,'%Y%m%d').date()
         self.period_end = datetime.strptime(period_end,'%Y%m%d').date()
 
-        self.data = yf.download(self.ticker, start=self.begin_c, end=self.end_c)
+        self.data = yf.download(self.ticker, start=self.period_begin, end=self.period_end)
         self.data = self.data['Adj Close']
 
     def get_name(self):
@@ -62,3 +63,13 @@ class ReturnAnalytics:
     def sharpe_ratio(self, risk_free_rate):
         sharpe_ratio = (100*self.data.pct_change().mean()-risk_free_rate)/np.std(self.data.pct_change())
         return sharpe_ratio
+
+    def plot_daily_returns(self):
+        daily_returns = self.data.pct_change()
+
+        fig, ax = plt.subplots()
+        ax.plot(daily_returns)
+        ax.set_ylabel('%s' % self.ticker)
+        ax.set_xlabel('Time')
+        fig.suptitle('Daily Returns')
+        fig.show()
